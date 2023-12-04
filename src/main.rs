62302431673d7e8de3453309a8884b8041174572
fruscianteee <redhot1205@gmail.com>
2023-12-05 00:30:43 +0900

    1.5.2

--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,26 @@ fn main() {
     // assert_eq!(a.as_ptr(), b.as_ptr());
     // thread::spawn(|| dbg!(a));
     // thread::spawn(|| dbg!(b));
-    f(&Cell::new(10), &Cell::new(20));
+    let a = RefCell::new(vec![10, 20]);
+    dbg!(&a);
+    f(&a);
+    dbg!(&a);
 }
 
-fn f(a: &Cell<i32>, b: &Cell<i32>) {
-    let before = a.get();
-    b.set(b.get() + 1);
-    let after = a.get();
-    if before != after {
-        x();
-    } else {
-        println!("aa");
-    }
+fn f(v: &RefCell<Vec<i32>>) {
+    v.borrow_mut().push(1);
 }
+
+// fn f(a: &Cell<i32>, b: &Cell<i32>) {
+//     let before = a.get();
+//     b.set(b.get() + 1);
+//     let after = a.get();
+//     if before != after {
+//         x();
+//     } else {
+//         println!("aa");
+//     }
+// }
 
 fn x() {
     println!("x");
